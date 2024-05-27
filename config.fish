5595dc9b22d95dc839698a1b69460d883b29dd4f
Felice Forgione <forgione67@gmail.com>
2024-05-26 23:30:48 -0400

    add config.fish

---
+++ config.fish
@@ -0,0 +1,9 @@
+clear
+fastfetch
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+fzf --fish | source
+zoxide init fish | source
